2ff8767fe0fdac41d8cddcada016385bb0f8ff8f
henan <976062158@qq.com>
2026-01-31 01:11:54 +0800

    fix: 配置 Vue Router 的 base path 为 /

--- src/router/index.js
+++ src/router/index.js
@@ -52,7 +52,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/'),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
