8036d45aa1b9f2cc771dfd469a1daf040169e285
henan <976062158@qq.com>
2026-01-31 01:52:46 +0800

    style: 移除了解更多，CSDN改博客，优化搜索框样式

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="relative">
+  <div class="relative w-full max-w-md mx-auto">
     <!-- 搜索输入框 -->
     <div class="relative">
       <input
         v-model="searchQuery"
         type="text"
         placeholder="搜索文章..."
-        class="w-full pl-10 pr-4 py-3 bg-gray-50 border border-gray-200 rounded-lg 
+        class="w-full pl-10 pr-10 py-2.5 bg-gray-50 border border-gray-200 rounded-lg 
                focus:outline-none focus:ring-2 focus:ring-primary/20 focus:border-primary
-               transition-all duration-300"
+               transition-all duration-300 text-base"
         @input="handleSearch"
         ref="searchInput"
       />
